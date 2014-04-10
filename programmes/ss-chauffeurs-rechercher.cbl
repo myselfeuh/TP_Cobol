31498237c6d1f9081235edd0cb320e7b39a3f560
Yann Lalaoui <yann.lalaoui@gmail.com>
2014-04-10 19:38:33 +0200

    arrange l'affichage des resultats et fait fonctionner la recherche dans les cas de resultats multiples et de zero resultat

--- programmes/ss-chauffeurs-rechercher.cbl
+++ programmes/ss-chauffeurs-rechercher.cbl
@@ -7,7 +7,7 @@
                access mode is dynamic
                    record key is numChaufN
                    alternate record key is nomN with duplicates
-               status FChaufNouvStatus.
+               status Fstatus.
 
        data division.
        file section.
@@ -19,7 +19,7 @@
                02 datePermisN  pic 9(8).
 
        working-storage section.
-       01 FChaufNouvStatus         pic x(2).
+       01 Fstatus         pic x(2).
        01 i                        pic 9(2).
        01 fin-fichier              pic 9.
        01 choix-type-recherche     pic 9.
@@ -42,24 +42,33 @@
            required.
 
        01 s-plg-recherche-id.
-           02 line 3 col 2 value 'Id du chauffeur: '.
+           02 line 3 col 2 value 'Id recherchee : '.
            02 s-id-chauffeur pic zzzz to id-chauffeur
            required.
        01 s-plg-recherche-nom.
-           02 line 3 col 2 value 'Nom du chauffeur: '.
+           02 line 3 col 2 value 'Nom recherche : '.
            02 s-nom-chauffeur pic x(30) to nom-chauffeur
            required.
 
+       01 a-plg-titre-colonne.
+           02 line 5 col 2 value 'Id'.
+           02 line 5 col 8 value 'Nom'.
+           02 line 5 col 39 value 'Prenom'.
+           02 line 5 col 69 value 'Date permis'.
+           02 line 6 col 1 value
+           '----------------------------------------------------------'
+               &'---------------------'.
        01 a-plg-chauffeur-data.
            02 a-numChaufN line i col 2    pic 9(4) from numChaufN.
            02 a-nomN line i col 8         pic x(30) from nomN.
-           02 a-prenomN line i col 23     pic x(30) from prenomN.
-           02 a-datePermisN line i col 36 pic 9(8) from datePermisN.
+           02 a-prenomN line i col 39     pic x(30) from prenomN.
+           02 a-datePermisN line i col 69 pic 9999/99/99
+               from datePermisN.
        01 a-plg-message-continuer.
-           02 line 20 col 1 value 'Appuyer sur une touche...'.
+           02 line 20 col 1 value 'Appuyez sur ENTREE pour continuer.'.
        01 a-plg-message-choix-invalide.
            02 line 20 col 1 value 'Choix invalide.'.
-       01 a-plg-message-aucun-resultat.
+       01 a-plg-mess-no-result.
            02 line i col 2 value 'Aucun resultat.'.
 
        01 a-plg-efface-ecran.
@@ -72,7 +81,7 @@
 
        open input FChaufNouv
 
-       move 5 to i
+       move 7 to i
        move 0 to numChaufN
 
        display a-plg-titre-global
@@ -110,15 +119,16 @@
            accept s-plg-recherche-id
 
            move id-chauffeur to numChaufN
-           start FChaufNouv key = numChaufN
+           display a-plg-titre-colonne
 
+           start FChaufNouv key = numChaufN
            read FChaufNouv
                invalid key
-                   display a-plg-message-aucun-resultat
+                   display a-plg-mess-no-result
                not invalid key
                    display a-plg-chauffeur-data
            end-read
-
+           display a-plg-message-continuer
            stop ' '
        .
 
@@ -128,22 +138,42 @@
            accept s-plg-recherche-nom
 
            move function upper-case(nom-chauffeur) to nomN
-           start FChaufNouv key = nomN
-
            move 0 to fin-fichier
 
-           read FChaufNouv next
-               at end
-                   display a-plg-chauffeur-data
+           display a-plg-titre-colonne
+
+           start FChaufNouv key = nomN
+           perform with test after until (
+               fin-fichier = 1 or
+               Fstatus = 46 or
+               ( Fstatus = '00' and
+                 nomN not = function upper-case(nom-chauffeur) )
+           )
+               read FChaufNouv next
+                   at end
+                       if (nomN = function upper-case(nom-chauffeur)
+                       ) then
+                           display a-plg-chauffeur-data
+                           display a-plg-message-continuer
+                           stop ' '
+                       end-if
+                       move 1 to fin-fichier
+                   not at end
+                       if (nomN = function upper-case(nom-chauffeur)
+                       ) then
+                           display a-plg-chauffeur-data
+                           display a-plg-message-continuer
+                           stop ' '
+                       end-if
+               end-read
+               if (Fstatus not = '00'
+                   and Fstatus not = '02'
+               ) then
+                   display a-plg-mess-no-result
+                   display a-plg-message-continuer
                    stop ' '
-               not at end
-                   display a-plg-chauffeur-data
-                   stop ' '
-           end-read
-           if FChaufNouvStatus not = '00' then
-               display a-plg-message-aucun-resultat
-               stop ' '
-           end-if
+               end-if
+           end-perform
        .
 
        REINITIALISER.
